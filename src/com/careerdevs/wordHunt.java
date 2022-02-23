129425a379aa5988a363b0814db1b0c7fca33213
wmaf <wmaf@outlook.com>
2022-02-23 14:33:52 -0500

    Homework

--- src/com/careerdevs/wordHunt.java
+++ src/com/careerdevs/wordHunt.java
@@ -14,27 +14,21 @@ public class wordHunt {
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Welcome to hidden word:");
 
-
         String hiddenWord = "pneumonoultramicroscopicsilicovolcanoconiosis";
 
         do {
             System.out.println("You have" +" "+ (10 - tries)+ " " + "tries remaining");
             System.out.println("please enter letter number:" + (tries + 1));
             String yourLetter = keyboard.nextLine();
-            //System.out.println(tries);
-            System.out.println(temp);
-
             for(int i = 0; i < hiddenWord.length(); i ++) {
                 if (yourLetter.equals(Character.toString(hiddenWord.charAt(i)))) {
                     if(!iterated)
                         temp += Character.toString(hiddenWord.charAt(i));
-
                     else {
                         holder = Character.toString(temp.charAt(i)).replace("-", yourLetter);
                         temp = temp.substring(0, i) + holder + temp.substring( i + 1, temp.length());
-
                     }
-                } else {
+                    } else {
                     if(!iterated) {
                         temp += "-";
                     }
