56b6552a2b82dcc43802d2c53468127fe2230041
wmaf <wmaf@outlook.com>
2022-02-24 15:25:44 -0500

    Homework

--- src/com/careerdevs/wordHuntMenu.java
+++ src/com/careerdevs/wordHuntMenu.java
@@ -1,20 +1,24 @@
 package com.careerdevs;
 
+import java.util.Scanner;
+
 public class wordHuntMenu {
     boolean exit;
+
     //Testing via public void
     public static void main(String[] args) {
         wordHuntMenu menu = new wordHuntMenu();
 //        menu.printHeader();
 //        menu.printMenu();
-    //End testing
+        //End testing
         menu.runMenu();
     }
 
     public void runMenu() {
         printHeader();
         while (!exit) {
-        printMenu();
+            printMenu();
+            int userChoice = getUserInput();
         }
     }
 
@@ -23,11 +27,25 @@ public class wordHuntMenu {
         System.out.println("|       Welcome to Word Hunt           |");
         System.out.println("*--------------------------------------*");
     }
+
     private void printMenu() {
         System.out.println("Guess the word by choosing a letter.");
         System.out.println("Please make a selection:");
         System.out.println("#(1) To play");
         System.out.println("#(2) To Quit");
+    }
 
+    private int getUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        int userSelection = -1;
+        while (userSelection < 0 || userSelection >= 2) {// use you variable here
+            try {
+                System.out.println("\nPlease make a selection:");
+                userSelection = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Something went wrong here, please try again");
+            }
+        }
+        return userSelection;
     }
 }
